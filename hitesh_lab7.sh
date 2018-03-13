0220f1b619c90aece59d27b78decb96c9b487c06
Hitesh kotte <35593884+hiteshkotte@users.noreply.github.com>
2018-03-13 14:28:49 +0530

    Create hitesh_lab7.sh

---
+++ hitesh_lab7.sh
@@ -0,0 +1,19 @@
+Aggarwal
+Agarwal
+Agrawal
+Aggrawal
+aggarwal
+agarwal
+aggrawal
+agrawal
+
+grep -i -e "Agarwal" -e "aggarwal" -e "agrawal" -e "aggarwal"-e "agarwal" hello.txt
+
+Result:
+
+hello.txt:Aggarwal
+hello.txt:Agarwal
+hello.txt:Agrawal
+hello.txt:aggarwal
+hello.txt:agarwal
+hello.txt:agrawal
