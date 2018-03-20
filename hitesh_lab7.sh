8d25486575dd0e8f29152d448431c964ff2e1d68
Hitesh kotte <35593884+hiteshkotte@users.noreply.github.com>
2018-03-20 14:40:03 +0530

    Update hitesh_lab7.sh

--- hitesh_lab7.sh
+++ hitesh_lab7.sh
@@ -1,5 +1,5 @@
 
- 1]
+Q1]
 Aggarwal
 Agarwal
 Agrawal
@@ -19,3 +19,45 @@ hello.txt:Agrawal
 hello.txt:aggarwal
 hello.txt:agarwal
 hello.txt:agrawal
+
+
+
+
+  
+Q2)
+------> cat hitesh1.txt
+Employee Name    Employee ID    Designation           Salary
+Hitesh           16UG08070      CEO                   150000
+Kishor           16UG08072      Admin                 50000
+Shashank         16UG08050      Manager               450000
+Vaibhava         16UG08065      Head                  300000
+Varsha           16UG08066      Employee              1500000
+
+
+
+Q3)
+-----> To replace "CEO" with "Head"
+ sed  's\Manager\Director\g;' hitesh1.txt
+Result:
+Employee Name    Employee ID    Designation           Salary
+Hitesh           16UG08070      Head                  150000
+Kishor           16UG08072      Admin                 50000
+Shashank         16UG08050      Manager               450000
+Vaibhava         16UG08065      CEO                   300000
+Varsha           16UG08066      Employee              1500000
+
+
+
+Q4)
+cat hitesh1.txt
+Employee Name    Employee ID    Designation           Salary
+Hitesh           16UG08070      Head                  150000
+Kishor           16UG08072      Admin                 50000
+Shashank         16UG08050      Manager               450000
+Vaibhava         16UG08065      CEO                   300000
+Varsha           16UG08066      Employee              1500000
+
+-----> To search for names that start with "v"
+$ grep -i ^v hitesh1.txt
+Vaibhava         16UG08065      CEO                   300000
+Varsha           16UG08066      Employee              1500000
